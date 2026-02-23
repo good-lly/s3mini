9ccf4f722e2ff5b7f048beb56da4e44ff1f8aef5
Jolly Good <1671375+good-lly@users.noreply.github.com>
2026-02-23 12:21:32 +0100

    update modules, dev deps & eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,9 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-export default defineConfig([
+export default defineConfig(
   globalIgnores([
     '**/.*',
     '/dist',
@@ -24,7 +13,6 @@ export default defineConfig([
     'eslint.config.js',
     'jest.config.js',
     'rollup.config.js',
-    'jest.config.js',
     'jest.public.js',
     'jest.config.perf.js',
     'tsconfig.json',
@@ -33,28 +21,16 @@ export default defineConfig([
     '.*.env',
     '.env',
   ]),
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  prettier,
   {
-    extends: compat.extends(
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      'prettier',
-    ),
-
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-
       parserOptions: {
-        project: './tsconfig.json',
+        projectService: true,
       },
     },
-
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'error',
       '@typescript-eslint/explicit-module-boundary-types': 'error',
@@ -63,18 +39,11 @@ export default defineConfig([
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/consistent-type-exports': 'error',
       '@typescript-eslint/no-floating-promises': 'error',
-
-      'no-console': [
-        'warn',
-        {
-          allow: ['warn', 'error'],
-        },
-      ],
-
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
       curly: 'error',
       eqeqeq: 'error',
       'no-var': 'error',
       'prefer-const': 'error',
     },
   },
-]);
+);
